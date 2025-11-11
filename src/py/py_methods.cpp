2d49f4c380d02c72bbe2e37a8db158aadbbf5bfd
Shane Mannion <shanemannion.ac@gmail.com>
2025-11-11 16:06:18 +0100

    beginning of code to wrap the oscillator class

--- src/py/py_methods.cpp
+++ src/py/py_methods.cpp
@@ -1,6 +1,6 @@
 #include "py_wrappers.h"
 
-PyMethodDef oscillatorFunctions[] = {
+PyMethodDef PyOscillatorFunctions[] = {
     {"kuramoto_model", py_kuramoto_model, METH_VARARGS, "Run Kuramoto model simulation"},
     {NULL, NULL, 0, NULL}  // Sentinel
 };
